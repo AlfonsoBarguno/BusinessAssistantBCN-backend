ad9ae5f0e721d992db9ccfae0a3c74c7270e160d
Alex Fernandez <alexfernandez@Sergios-MacBook-Pro.local>
2023-03-29 10:30:08 +0200

    delete getDataByYear

--- BusinessAssistant-gencat/src/main/java/com/businessassistantbcn/gencat/service/RaiscService.java
+++ BusinessAssistant-gencat/src/main/java/com/businessassistantbcn/gencat/service/RaiscService.java
@@ -2,7 +2,6 @@ package com.businessassistantbcn.gencat.service;
 
 import com.businessassistantbcn.gencat.config.PropertiesConfig;
 import com.businessassistantbcn.gencat.dto.GenericResultDto;
-import com.businessassistantbcn.gencat.dto.io.CcaeDto;
 import com.businessassistantbcn.gencat.dto.output.RaiscResponseDto;
 import com.businessassistantbcn.gencat.dto.output.ResponseScopeDto;
 import com.businessassistantbcn.gencat.helper.JsonHelper;
@@ -10,8 +9,6 @@ import com.businessassistantbcn.gencat.helper.RaiscDeserializer;
 import com.businessassistantbcn.gencat.proxy.HttpProxy;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -20,10 +17,7 @@ import reactor.core.publisher.Mono;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 @Service
 public class RaiscService {
@@ -54,28 +48,6 @@ public class RaiscService {
                             .toArray(RaiscResponseDto[]::new);
                     return Mono.just(responses);
                 });
-    }
-
-    private Mono<RaiscResponseDto[]> getDataByYear(String year) throws MalformedURLException {
-         /*return getData().flatMap(raiscResponseDto->{
-             Arrays.stream(raiscResponseDto).filter(raiscRe.getAnyo().equals(year).toArray(RaiscResponseDto[]::new);
-                        //Arrays.stream(raiscResponseDto).filter(a->Integer.parseInt(a.getAnyo()) == Integer.parseInt(year)).toArray(RaiscResponseDto[]::new);
-                        //Arrays.stream(raiscResponseDto).filter(a->a.getAnyo() == year).toArray(RaiscResponseDto[]::new);
-                        //Arrays.stream(raiscResponseDtos).filter(raiscResponseDto->raiscResponseDto.getAnyo().equals(year)).toArray(RaiscResponseDto[]::new);
-                    //Arrays.stream(raiscResponseDtos).anyMatch(year).map(raiscResponseDto -> {raiscResponseDto.getAnyo().equals(year);)});
-                    return Mono.just(response);
-                });*/
-        /*return httpProxy.getRequestData(new URL(config.getDs_raisc()), Object.class)
-                .flatMap(raiscResponseDto -> {
-                    RaiscResponseDto[] responses = Arrays.stream(raiscDeserializer.deserialize(raiscResponseDto)
-                            .toArray(RaiscResponseDto[]::new)).filter(a->a.getAnyo().equals(year)).toArray(RaiscResponseDto[]::new);
-                    RaiscResponseDto[] response = Arrays.stream(responses).filter(a->a.getAnyo().equals(year)).toArray(RaiscResponseDto[]::new);
-                    return Mono.just(response);
-                });*/
-
-        /*RaiscResponseDto[] dataByYear = getData().block();
-        dataByYear = Arrays.stream(dataByYear).anyMatch(a->Integer.parseInt(a.getAnyo())==Integer.parseInt(year));//.toArray(RaiscResponseDto[]::new);
-        return Mono.just(dataByYear);*/
     }
 
     private Mono<GenericResultDto<RaiscResponseDto>> getRaiscDefaultPage() {
